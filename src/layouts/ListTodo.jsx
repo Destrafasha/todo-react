eb464aabb465f0223215a9ec7b901bab64e83b7d
destra fasha <destra.fasha@gmail.com>
2025-09-24 17:59:00 +0700

    menambahkan fitur Done & Undo di todo list

--- src/layouts/ListTodo.jsx
+++ src/layouts/ListTodo.jsx
@@ -4,10 +4,20 @@ const ListTodo = ({ todos, setTodo, setIndexEdit }) => {
     setTodo(newTodo);
   };
 
-
   const editTodo = (currentIndex) => {
     setIndexEdit(currentIndex);
-  }
+  };
+
+  // ✅ Tambahan baru: fungsi doneTodo
+  const doneTodo = (currentIndex) => {
+    const newTodo = todos.map((todo, index) => {
+      if (index === currentIndex) {
+        return { ...todo, isDone: !todo.isDone }; // toggle
+      }
+      return todo;
+    });
+    setTodo(newTodo);
+  };
 
   return (
     <>
@@ -16,18 +26,28 @@ const ListTodo = ({ todos, setTodo, setIndexEdit }) => {
           {todos.map((todo, index) => {
             return (
               <li key={index} className="m-[20px]">
-                - {todo.title}{" "}
+                -{" "}
+                <span className={todo.isDone ? "line-through" : ""}>
+                  {todo.title}
+                </span>{" "}
                 <button
                   onClick={() => editTodo(index)}
-                  className="bg-yellow-600 p-20px mr-[10px]"
+                  className="bg-yellow-600 px-[10px] py-[5px] mr-[10px]"
                 >
                   Edit
                 </button>
                 <button
                   onClick={() => deleteTodo(index)}
-                  className="bg-red-600 p-20px"
+                  className="bg-red-600 px-[10px] py-[5px] mr-[10px]"
                 >
                   Hapus
+                </button>
+                {/* ✅ Tambahan baru: tombol Done */}
+                <button
+                  onClick={() => doneTodo(index)}
+                  className="bg-green-600 px-[10px] py-[5px]"
+                >
+                  {todo.isDone ? "Undo" : "Done"}
                 </button>
               </li>
             );
